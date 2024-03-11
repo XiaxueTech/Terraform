93a96161dec93c568a8937b1e816f7ca6447e921
Elena Huma <85832851+IoanaElenaH@users.noreply.github.com>
2024-03-11 16:14:27 +0200

    add main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+data "aws_vpc" "test1" {
+    default = true
+}
+
+resource "aws_security_group" "test" {
+    name        = "test"
+    description = "Allow inbound"
+    vpc_id      = data.aws_vpc.test1.id
+}
+
+resource "aws_security_group_rule" "test" {
+    type        = "ingress"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = var.public_cidr_block
+    security_group_id = "${aws_security_group.test.id}"
+}
