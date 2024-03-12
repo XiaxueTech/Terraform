ebe431c8a43ffd03d83983755a974659b5964816
Elena Huma <85832851+IoanaElenaH@users.noreply.github.com>
2024-03-12 17:46:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,3 +31,12 @@ resource "aws_security_group_rule" "egress" {
     cidr_blocks = [0.0.0.0/0]
     security_group_id = "${aws_security_group.egress.id}"
 }
+
+resource "aws_security_group_rule" "egress2" {
+    type        = "egress2"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [0.0.0.0/0]
+    security_group_id = "${aws_security_group.egress.id}"
+}
