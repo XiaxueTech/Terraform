b590e9711d0b59ae9982aacf9503b115fbdf30f7
Ioana <ioana.huma@levi9.com>
2024-03-15 15:55:37 +0200

    update

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,10 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket"
-    key            = "dev-state/terraform.tfstate"
-    region         = "eu-west-1"
-    encrypt        = true
-    dynamodb_table = "tfstate-lock"
-    role_arn       = "arn:aws:iam::00000000000:role/rolename"
+  backend "local" {
   }
 }
