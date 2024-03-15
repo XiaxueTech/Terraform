e1aa42c263a25c73530b726d4b627b828cbd4467
Elena Huma <85832851+IoanaElenaH@users.noreply.github.com>
2024-03-15 20:38:54 +0200

    add example.tf

---
+++ env/dev/example.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "example_sg" {
+  name        = "example-sg"
+  description = "Example security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  resource "aws_security_group" "example_sg_0" {
+  name        = "example-sg-0"
+  description = "Example security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
