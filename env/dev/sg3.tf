91a29061f61a5de0e91eab7dd13a5b479bdd4826
Elena Huma <85832851+IoanaElenaH@users.noreply.github.com>
2024-03-15 17:28:56 +0200

    add sg3.tf

---
+++ env/dev/sg3.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "incorrect_security_group" {
+  name        = "incorrect-security-group"
+  description = "Incorrect Security Group"
+
+  // Restricting
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  // Allowing Any IP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Allowing All Traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
