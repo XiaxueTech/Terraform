846a8db8953d0fd811611c5179a9c2507a79910e
Elena Huma <85832851+IoanaElenaH@users.noreply.github.com>
2024-03-17 22:28:15 +0200

    Update example.tf

--- env/dev/example.tf
+++ env/dev/example.tf
@@ -33,3 +33,15 @@ resource "aws_security_group" "example_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+  resource "aws_security_group" "example_sg_2" {
+  name        = "example-sg-2"
+  description = "Example security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
